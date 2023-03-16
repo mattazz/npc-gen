658bf97ad883fc1b89efea8fb8e3d7c9ec46b08b
Mattazz <matthewazada101@gmail.com>
2023-03-16 14:31:44 +0800

    f

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter  as Router, Route, Switch} from 'react-router-dom'
+import { HashRouter, Route, Routes} from 'react-router-dom'
 import App from './App'
 import Header from './assets/Components/Header'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-        <App />
+        <StrictMode>
+                 <HashRouter>
+                         <App />
+                </HashRouter>    
+        </StrictMode>
 )
