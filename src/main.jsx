da584fd979ac20aab90d93a37a1d36d8d17a64a4
Mattazz <matthewazada101@gmail.com>
2023-03-16 13:17:53 +0800

    configs for gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter  as Router, Route, Switch} from 'react-router-dom'
 import App from './App'
 import Header from './assets/Components/Header'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <StrictMode>
         <App />
-    </StrictMode>
 )
